ecfdbdd63574a72039e7a5eb82e8513679015382
Naved <naved1234@gmail.com>
2018-09-07 19:25:31 +0530

    code additions

--- public/js/index.js
+++ public/js/index.js
@@ -14,12 +14,40 @@ $(document).ready(() => {
     }));
 
     // obtain users service
-    const usersService = client.service('/users');
+		const usersService = client.service('/users');
+		const messagesService = client.service('/messages');
+
+		class Message {
+			constructor(message) {
+				this.message = message;
+			}
+			getMessageHtmlString() {
+				return `
+					<div class="media">
+					<div class="media-left">
+							<a href="#">
+									<img src="https://www.iconexperience.com/_img/o_collection_png/green_dark_grey/512x512/plain/user.png" alt="64x64 user image" class="media-object" style="width: 64px; height: 64px;">
+							</a>
+					</div>
+					<div class="media-body">
+							<div class="pull-right">
+									<span class="delete-comment" title="Delete Comment?"><i class="fa fa-times" aria-hidden="true"></i></span>
+							</div>
+							<h4 class="media-heading">John Smith</h4>
+							<span class="comment-date">03-04-2016 10:43am</span>
+							<br><br>
+							${this.message}
+					</div>
+			</div>
+				`
+			}
+ 		}
 
     const authenticate = async() => {
         try {
             const response = await client.authenticate();
             if(response) {
+
                 // Logout the user if logout button is clicked
                 $('#logout-icon').on('click', async() => {
                     try {
@@ -29,11 +57,37 @@ $(document).ready(() => {
                     catch(error) {
                         console.error(error.message);
                     }
-                })
+								});
+								
+								// save the message to database if user enters one
+								$('#submit-message-form').submit(async(e) => {
+									e.preventDefault();
+									const $msgElement = $('#msg-text');
+									const msg = $msgElement.val();
+									$msgElement.val('');
+									if(msg.trim().length) {
+										try {
+											const response = await messagesService.create({
+												text: msg
+											});
+										}
+										catch(error) {
+											console.log(error.message);
+											alert(error.message);
+										}
+									} else {
+										alert('Enter a Valid message');
+									}
+								});
+
+								// event to check addition of new messages
+								messagesService.on('created', (message) => {
+									const htmlMessage = new Message(message.text);
+									$('#chat-area').append(htmlMessage.getMessageHtmlString());
+								});
             }
         }
         catch(error) {
-            console.error(error);
             window.location.href = `${serverUrl}/login.html`;
         }
 		}
